f6cfc75d3eeb58659ff7347ce9bc39aed077aa81
bunkatu <batuhancoskun@yandex.com>
2016-01-29 19:47:08 +0200

    Initial Commit

---
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.game.GGJ16;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GGJ16(), config);
+	}
+}
