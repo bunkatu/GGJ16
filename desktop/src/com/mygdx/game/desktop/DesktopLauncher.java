0fa11a54fc3d5ff8100e313113537d0d47c9b97c
Alim Giray Aytar <alimgiray@gmail.com>
2016-01-29 22:06:23 +0200

    Background Screen Images & Screen Width/Height added

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.game.GGJ16;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		config.title = "GameTitle";
 		new LwjglApplication(new GGJ16(), config);
 	}
 }
